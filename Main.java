3ffef2d4b89fec01970d07e3576b9daf3e5d043c
Ahmet Emre Altındal <220513377@ogr.altinbas.edu.tr>
2026-04-03 21:51:24 +0200

    i and j: added sorting algorithm

--- Main.java
+++ Main.java
@@ -1,7 +1,18 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         int[] table = new int[5];
-        System.out.println("Table elements:");
+        Random rand = new Random();
+        
+        System.out.println("Table with random values:");
+        for (int i = 0; i < table.length; i++) {
+            table[i] = rand.nextInt(100);
+            System.out.println("Index " + i + ": " + table[i]);
+        }
+        Arrays.sort(table);
+        System.out.println("\nSorted table:");
         for (int i = 0; i < table.length; i++) {
             System.out.println("Index " + i + ": " + table[i]);
         }
