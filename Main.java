9d6dfecd5f9b5419fa628aa0b79ff8c660f5f958
Ahmet Emre Altındal <220513377@ogr.altinbas.edu.tr>
2026-04-03 21:41:35 +0200

    g and h: added random values to table

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] table = new int[5];
+        System.out.println("Table elements:");
+        for (int i = 0; i < table.length; i++) {
+            System.out.println("Index " + i + ": " + table[i]);
+        }
+    }
+}
